da23b94cd6390c22b38ef47ce459d79e69ec166e
bhavikagandotra <bhavikagandotra66@gmail.com>
2023-09-04 11:35:45 +0530

    uploaded

---
+++ script.js
@@ -0,0 +1,8 @@
+function open() {
+    console.log("hello test");
+    document.getElementById("blywd_mash").classList.remove('d-none');
+  }
+  
+function demoVisibility() {
+    document.getElementById("blywd_mash").classList.remove("show");
+}
